bbcde6ea2937256bae27c406b2731db005b96151
rvpatil1432 <rvpatil1432@gmail.com>
2021-12-09 23:40:57 +0530

    Refactor : Changes done to find max using generics

--- src/com/bredgelabz/generics/Maximum.java
+++ src/com/bredgelabz/generics/Maximum.java
@@ -1,95 +1,41 @@
 package com.bredgelabz.generics;
+import static org.junit.Assert.assertEquals;
+
 import java.util.Scanner;
 
 
 
-public class Maximum {
-	/*
-	 * UC1 : Given 3 Integers find the maximum
-	 */
-	public int largestInt(Integer x,Integer y,Integer z) 
-	{
-		if(x.compareTo(y) > 0 && x.compareTo(z) > 0)
-		{
-			return x;
-		}
-		else if(y > z)
-		{
-			return y;
-		}
-		else
-		{
-			return z;
-		}
+public class Maximum<T extends Comparable<T>> {
+	T x,y,z;
 
+	public Maximum(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
-	/*
-	 * UC2 : Given 3 Float find the maximum
-	 */
-	public Float largestFloat(Float x,Float y,Float z) 
-	{
-		if(x.compareTo(y) > 0 && x.compareTo(z) > 0)
-		{
-			return x;
-		}
-		else if(y > z)
-		{
-			return y;
-		}
-		else
-		{
-			return z;
-		}
-
+	public T Maximum() {
+		return Maximum.maximum(x, y, z);
 	}
-	/*
-	 * UC3 : Given 3 String find the maximum
-	 */
-	public String largestString(String x,String y,String z) 
-	{
-		Integer s1 = x.length(); 
-		Integer s2 = y.length(); 
-		Integer s3 = z.length(); 
-		if(s1.compareTo(s2) > 0 && s1.compareTo(s3) > 0)
-		{
-			return x;
+	public static <T extends Comparable<T>> T maximum(T x, T y, T z){
+		T max = x;
+		if(y.compareTo(max) > 0 ) {
+			max = y;
 		}
-		else if(s2.compareTo(s3) > 0)
-		{
-			return y;
+		if(z.compareTo(max) > 0) {
+			max = z;
 		}
-		else
-		{
-			return z;
-		}
-
+		printMax(x, y, z, max);
+		return max;
+	}
+	public static <T> void printMax(T x,T y,T z,T max){
+		System.out.println("Max is "+max);
 	}
 	public static void main(String[] args) {
-		Integer x, y, z;
-		Float a,b,c;
-		String str1,str2,str3;
-		Scanner s = new Scanner(System.in);
-		System.out.print("Enter the first integer number:");
-		x = s.nextInt();
-		System.out.print("Enter the second integer number:");
-		y = s.nextInt();
-		System.out.print("Enter the third integer number:");
-		z = s.nextInt();
-		Maximum obj = new Maximum();
-		int max = obj.largestInt(x,y,z);
-		System.out.print("Enter the first float number:");
-		a = s.nextFloat();
-		System.out.print("Enter the second float number:");
-		b = s.nextFloat();
-		System.out.print("Enter the third float number:");
-		c = s.nextFloat();
-		float maxFloat = obj.largestFloat(a,b,c);
-		System.out.print("Enter the string one:");
-		str1 = s.next();
-		System.out.print("Enter the string two:");
-		str2 = s.next();
-		System.out.print("Enter the string three:");
-		str3 = s.next();
-		String maxStr = obj.largestString(str1,str2,str3);
+		Integer xInt = 3, yInt = 4, zInt = 5;
+		Float xF1 = 6.6f, yF1 = 8.8f, zF1 = 7.7f;
+		String xStr = "Pear", yStr = "Apple", zStr = "Orange"; 
+		new Maximum(xInt, yInt, zInt).Maximum();
+		new Maximum(xF1, yF1, zF1).Maximum();
+		new Maximum(xStr, yStr, zStr).Maximum();
 	}
 }
--- src/com/bredgelabz/generics/MaximumTest.java
+++ src/com/bredgelabz/generics/MaximumTest.java
@@ -8,78 +8,77 @@ import org.junit.Test;
 
 public class MaximumTest {
 	private static final double DELTA = 1e-15;
-	Maximum obj = new Maximum();
+	
 	/*
 	 * TC1 : Given Max Number at 1st Position return the Same Number
 	 */
 	@Test
 	public void largestNumAtFirstPos(){
-		
-		int result = obj.largestInt(40, 30, 20);
-		assertEquals(40, result,DELTA);
+		Comparable result = new Maximum(40, 30, 20).Maximum();
+		assertEquals(40, result);
 	}
 	/*
 	 * TC2 : Given Max Number at 2nd Position return the Same Number
 	 */
 	@Test
 	public void largestNumAtSecondPos(){
-		int result = obj.largestInt(30, 40, 20);
-		assertEquals(40, result,DELTA);
+		Comparable result = new Maximum(30, 40, 20).Maximum();
+		assertEquals(40, result);
 	}
 	/*
 	 * TC3 : Given Max Number at 3rd Position return the Same Number
 	 */
 	@Test
 	public void largestNumAtThirdPos(){
-		int result = obj.largestInt(20, 30, 40);
-		assertEquals(40, result,DELTA);
+		Comparable result = new Maximum(40, 30, 20).Maximum();
+		assertEquals(40, result);
 	}
 	/*
 	 * TC1 : Given Max Float Number at 1st Position return the Same Number
 	 */
 	@Test
 	public void largestFloatAtFirstPos(){
-		float result = obj.largestFloat(40f, 30f, 20f);
-		assertEquals(40f, result,DELTA);
+		Comparable result = new Maximum(40f, 30f, 20f).Maximum();
+		assertEquals(40f, result);
 	}
 	/*
 	 * TC2 : Given Max Float Number at 2nd Position return the Same Number
 	 */
 	@Test
 	public void largestFloatAtSecondPos(){
-		float result = obj.largestFloat(30f, 40f, 20f);
-		assertEquals(40f, result,DELTA);
+		Comparable result = new Maximum(30f, 40f, 20f).Maximum();
+		assertEquals(40f, result);
 	}
 	/*
 	 * TC3 : Given Max Float Number at 3rd Position return the Same Number
 	 */
 	@Test
 	public void largestFloatAtThirdPos(){
-		float result = obj.largestFloat(20f, 30f, 40f);
-		assertEquals(40f, result,DELTA);
+		Comparable result = new Maximum(20f, 30f, 40f).Maximum();
+		assertEquals(40f, result);
 	}
 	/*
 	 * TC1 : Given String at 1st Position return the Same String
 	 */
 	@Test
 	public void largestStringAtFirstPos(){
-		String result = obj.largestString("Banana", "Apple", "Peach");
-		assertEquals("Banana", result);
+		Comparable result = new Maximum("Banana", "Apple", "Peach").Maximum();
+		assertEquals("Peach", result);
 	}
 	/*
 	 * TC2 : Given Max String at 2nd Position return the Same String
 	 */
 	@Test
 	public void largestStringAtSecondPos(){
-		String result = obj.largestString("Apple", "Banana", "Peach");
-		assertEquals("Banana", result);
+		Comparable result = new Maximum("Apple", "Banana", "Peach").Maximum();
+		assertEquals("Peach", result);
 	}
 	/*
 	 * TC3 : Given Max String at 3rd Position return the Same String
 	 */
 	@Test
 	public void largestStringAtThirdPos(){
-		String result = obj.largestString("Apple", "Peach", "Banana");
-		assertEquals("Banana", result);
+		Comparable result = new Maximum("Apple", "Peach", "Banana").Maximum();
+		assertEquals("Peach", result);
 	}
 }
