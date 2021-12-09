46e9a3c7ee7712d311aec5227fdb47d8d3b1d2a9
rvpatil1432 <rvpatil1432@gmail.com>
2021-12-09 14:49:31 +0530

    Adding : UseCse2 to check max float

--- src/com/bredgelabz/generics/Maximum.java
+++ src/com/bredgelabz/generics/Maximum.java
@@ -22,8 +22,28 @@ public class Maximum {
 		}
 
 	}
+	/*
+	 * UC2 : Given 3 Float find the maximum
+	 */
+	public Float largestFloat(Float x,Float y,Float z) 
+	{
+		if(x.compareTo(y) > 0 && x.compareTo(z) > 0)
+		{
+			return x;
+		}
+		else if(y > z)
+		{
+			return y;
+		}
+		else
+		{
+			return z;
+		}
+
+	}
 	public static void main(String[] args) {
 		Integer x, y, z;
+		Float a,b,c;
 		Scanner s = new Scanner(System.in);
 		System.out.print("Enter the first integer number:");
 		x = s.nextInt();
@@ -33,5 +53,12 @@ public class Maximum {
 		z = s.nextInt();
 		Maximum obj = new Maximum();
 		int max = obj.largestInt(x,y,z);
+		System.out.print("Enter the first float number:");
+		a = s.nextFloat();
+		System.out.print("Enter the second float number:");
+		b = s.nextFloat();
+		System.out.print("Enter the third float number:");
+		c = s.nextFloat();
+		float maxFloat = obj.largestFloat(a,b,c);
 	}
 }
--- src/com/bredgelabz/generics/MaximumTest.java
+++ src/com/bredgelabz/generics/MaximumTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 
 
+
 public class MaximumTest {
 	private static final double DELTA = 1e-15;
 	Maximum obj = new Maximum();
@@ -32,5 +33,29 @@ public class MaximumTest {
 	public void largestNumAtThirdPos(){
 		int result = obj.largestInt(20, 30, 40);
 		assertEquals(40, result,DELTA);
+	}
+	/*
+	 * TC1 : Given Max Float Number at 1st Position return the Same Number
+	 */
+	@Test
+	public void largestFloatAtFirstPos(){
+		float result = obj.largestFloat(40f, 30f, 20f);
+		assertEquals(40f, result,DELTA);
+	}
+	/*
+	 * TC2 : Given Max Float Number at 2nd Position return the Same Number
+	 */
+	@Test
+	public void largestFloatAtSecondPos(){
+		float result = obj.largestFloat(30f, 40f, 20f);
+		assertEquals(40f, result,DELTA);
+	}
+	/*
+	 * TC3 : Given Max Float Number at 3rd Position return the Same Number
+	 */
+	@Test
+	public void largestFloatAtThirdPos(){
+		float result = obj.largestFloat(20f, 30f, 40f);
+		assertEquals(40f, result,DELTA);
 	}
 }
